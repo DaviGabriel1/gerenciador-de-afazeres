6a88bad2c98456d1c3c01add519f035b97df0870
DaviGabriel1 <davig41441503@gmail.com>
2025-02-01 19:59:39 -0300

    feat: mongoDB atlas

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
+import {config} from "dotenv";
 
-const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/mongoBase";
+config();
+
+const MONGO_URI = process.env.MONGO_URI ? process.env.MONGO_URI : "mongodb://localhost:27017/mongoBase";
 
 export const connectDB = async () => {
     try{
-        await mongoose.connect(MONGO_URI,{
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        } as mongoose.ConnectOptions);
+        await mongoose.connect(MONGO_URI);
         console.log("conectado ao mongoDB com sucesso!");
     }
     catch(error){
